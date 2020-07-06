6b519bd679e81c364142eb732cd5b83b59329e02
dgkn94 <dgknonal@hotmail.com>
2020-07-06 19:05:32 +0300

    go

--- def.py
+++ def.py
@@ -14,12 +14,40 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 print("Kutuphane basariliyla yuklendi")
-
+print("_____________________________")
 # DATASETI URL'DEN CEK VE CVS HALINDE OKU
 try:
     url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
     isim = ["sepal-uzunluk", "sepal-genislik", "petal-uzunluk", "petal-genislik","sinif"]
     dataset = read_csv(url, names=isim)
     print("cvs dosyasi okuma basarili.")
+    print("_____________________________")
 except Exception as e:
     print("cvs dosyasi okunamadı")
+    print("_____________________________")
+
+#DATASETIN Boyutunu
+print("Dataset'in boyutu :" + str(dataset.shape))
+print("_____________________________")
+
+##DATASETIN 20 NESNESINE BAK
+## print(dataset.head(20))
+
+##DATASETIN ISTATIKSEL OZETI
+print(dataset.describe())
+print("_____________________________")
+
+## SINIF DAGILIMI
+print(dataset.groupby('sinif').size())
+print("_____________________________")
+
+##DATA GORSELLEME KUTU GRAFİĞİ
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False,sharey=False)
+
+#HISTOGRAM GRAFİĞİ
+#dataset.hist()
+#pyplot.show()
+
+#SCATTER MATRIX GRAFIGI
+#scatter_matrix(dataset)
+#pyplot.show()
