d6cbaffcc89528a8352cca22de266c53bcd06cb1
dgkn94 <dgknonal@hotmail.com>
2020-07-08 16:17:14 +0300

    YOLLA

--- knn.py
+++ knn.py
@@ -9,6 +9,29 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
 
-print("Kutuphane yuklendi.")
-print("_____________________")
+hava = ["Gunesli", "Yagmurlu", "Bulutlu","Bulutlu","Bulutlu","Yagmurlu","Yagmurlu","Gunesli","Yagmurlu","Bulutlu","Yagmurlu","Gunesli","Bulutlu","Yagmurlu","Gunesli"]
+sicaklik = ["Sicak","Soguk","Nemli", "Nemli", "Nemli", "Soguk", "Soguk", "Sicak", "Soguk", "Nemli", "Soguk", "Sicak","Nemli","Soguk","Sicak"]
+sokagaCik = ["Evet","Hayir","Evet","Evet","Evet","Hayir","Hayir","Evet","Hayir","Evet","Hayir","Evet","Evet","Hayir","Evet"]
+# 0 bulut 1 gunes 2 Yagmurlu
+# 0 nemli 1 sicak 2 Soguk
+# 0 evet 1 hayir
+encode = LabelEncoder()
+
+hava_enc = encode.fit_transform(hava)
+sicaklik_enc = encode.fit_transform(sicaklik)
+sokagaCik = encode.fit_transform(sokagaCik)
+print(hava_enc)
+print(sicaklik_enc)
+print(sokagaCik)
+
+ozellikler = list(zip(hava_enc, sicaklik_enc))
+
+
+model = KNeighborsClassifier(n_neighbors = 3)
+model.fit(ozellikler,sokagaCik)
+
+
+tahmin = model.predict([[1,2]])
+print(tahmin)
